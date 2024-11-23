d7d69323e3d16a3dc5e8080a1a762e2134841745
421budhi <124257480+budhi421@users.noreply.github.com>
2024-11-23 17:29:14 +0530

    1simple.java

--- simple.java
+++ simple.java
@@ -1,5 +1,6 @@
-class simple{
-public static void main(String args[]){
-System.out.println("Welcome to cloud knowledge");
+class simple {  // Declares a class named 'simple'
+    public static void main(String args[]) {  // Main method, entry point of the program
+        System.out.println("Welcome to cloud knowledge");  // Prints the message to the console
+    }
 }
-}
+
