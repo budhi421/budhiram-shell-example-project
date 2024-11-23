b475407853b40b2acc8f0cd05c441e36de846a62
421budhi <124257480+budhi421@users.noreply.github.com>
2024-11-23 17:37:28 +0530

    Update simple.java

--- simple.java
+++ simple.java
@@ -1,6 +1,6 @@
 class simple {  // Declares a class named 'simple'
     public static void main(String args[]) {  // Main method, entry point of the program
-        System.out.println("Welcome to cloud knowledge");  // Prints the message to the console
+        System.out.println("Welcome to cloud");  // Prints the message to the console
     }
 }
 
